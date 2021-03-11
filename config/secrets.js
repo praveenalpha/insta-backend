45410f51a9cf0df8422f91b4b79c9d0178680278
Praveen Mani <48654802+praveenalpha@users.noreply.github.com>
2021-03-11 22:38:28 +0530

    Add files via upload

---
+++ config/secrets.js
@@ -0,0 +1,4 @@
+module.exports = {
+    "CLIENT_ID": "1099356999168-82r2atri81r8vk2nft1bs9vv7gfgkbaf.apps.googleusercontent.com",
+    "CLIENT_PW": "85PbVl2iO1tsbP6JP3UojjM1"
+}
---
+++ secret.js
@@ -0,0 +1,13 @@
+const secret = {
+    Host:"b4kmvne0ewx2vo28ril1-mysql.services.clever-cloud.com",
+    DB_name:"b4kmvne0ewx2vo28ril1",
+    pass:"6oiSGkKyQ6rEcpScx2RE",
+    user:"u40ejijkcxa5wdkw"
+}
+module.exports = secret;
+
+
+
+// https://git.heroku.com/peaceful-wildwood-03590.git
+
+// https://peaceful-wildwood-03590.herokuapp.com/
